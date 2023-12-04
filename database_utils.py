1099f1cfcacd6a54145dfae16487b10df67ab86f
Dmitry Rukazenkov <dmitry@pc-11-251.customer.ask4.lan>
2023-12-04 19:37:19 +0000

    updated database utils updated database utils

--- database_utils.py
+++ database_utils.py
@@ -11,43 +11,34 @@ class DatabaseConnector:
     #def __init__(self) -> None:
         #pass
 
-    def read_db_creds(self):
-        with open('db_creds.yaml','r') as stream:
+    def read_db_creds(self,cred_path):
+        with open(cred_path,'r') as stream:
             cred = yaml.safe_load(stream)
             print(cred)
             return cred
 
 
     def init_db_engine(self,cred):
-        engine = create_engine(f"{'postgresql'}+{'psycopg2'}://{cred['RDS_USER']}:{cred['RDS_PASSWORD']}@{cred['RDS_HOST']}:{cred['RDS_PORT']}/{cred['RDS_DATABASE']}")
+        engine = create_engine(f"{'postgresql'}+{'psycopg2'}://{cred['USER']}:{cred['PASSWORD']}@{cred['HOST']}:{cred['PORT']}/{cred['DATABASE']}")
         engine.connect()
         return engine
 
     def list_db_tables(self,engine):
         inspector = inspect(engine)
         return inspector.get_table_names()    
+    
+    def upload_to_db(self,df,table,sql_engine):
+        df.to_sql(table,sql_engine ,if_exists='replace')
 
 
+if __name__ == '__main__':
+    instance = DatabaseConnector()
+    creds = instance.read_db_creds('db_creds.yaml')
+    engine = instance.init_db_engine(creds)
+    engine.connect()
+    table_list = instance.list_db_tables(engine)
+    print(table_list)
+    # with engine.connect() as conn:
+    #     table=pd.read_sql_table(table_list[1],con=conn)
+    #     print(table)
 
-instance = DatabaseConnector()
-creds = instance.read_db_creds()
-engine = instance.init_db_engine(creds)
-engine.connect()
-table_list = instance.list_db_tables(engine)
-print(table_list)
-with engine.connect() as conn:
-    table=pd.read_sql_table(table_list[1],con=conn)
-    print(table)
-
-#variable = DatabaseConnector()
-#variable.read_db_creds()
-
-
-
-    #def list_db_tables():    
-    #SELECT * FROM 
-        
-    #def read_rds_table():
-
-    #upload_to_db
-    #dim users

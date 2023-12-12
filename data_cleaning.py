2dda0493a89b3732d5454efb67dcb403ddb1be10
Dmitry Rukazenkov <dmitry@Dmitrys-MacBook-Air.local>
2023-12-12 12:11:27 +0000

    added data cleaning and data extraction

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,1 +1,80 @@
-class DataCleaning
+import pandas as pd
+import numpy as np
+import re
+from pandasgui  import show
+
+pd.set_option('display.max_columns', None)
+# df.info()
+
+class DataCleaning:
+    def clean_invalid_date(self,df: pd.DataFrame,column_name):
+        df[column_name] = pd.to_datetime(df[column_name], format='%Y-%m-%d', errors='ignore')
+        df.dropna(subset = column_name,how='any',inplace= True)
+        print(type(df))
+        print(df.columns)
+        return df
+    
+    def clean_user_data(self,df):
+        df = self.clean_invalid_date(df,'date_of_birth')
+        df = self.clean_invalid_date(df,'join_date')        
+        df.dropna(how='any',inplace=True)
+        df.drop(columns='index',inplace=True)
+        df = df.loc[~(df['first_name'] == 'NULL')]
+        df = df.loc[~(df['first_name'].str.contains('\d', regex=True, case=True))]
+        return df
+    
+    
+    def clean_card_data(self,df):
+        df['card_number'] = df['card_number'].apply(str)
+        df['card_number'] = df['card_number'].str.replace('?','')
+        df = self.clean_invalid_date(df,'date_payment_confirmed')  
+        df.dropna(how='any',inplace= True)
+        return df
+
+    def clean_date_time(self,df):
+        df['month']         =  pd.to_numeric( df['month'],errors='coerce', downcast="integer")
+        df['year']          =  pd.to_numeric( df['year'], errors='coerce', downcast="integer")
+        df['day']           =  pd.to_numeric( df['day'], errors='coerce', downcast="integer")
+        df['timestamp']     =  pd.to_datetime(df['timestamp'], format='%H:%M:%S', errors='coerce')
+        df.dropna(how='any',inplace= True)
+        df.reset_index(inplace=True)       
+        return df
+
+    def clean_products_weights(self,df):
+        df = self.clean_invalid_date(df,'date_added')
+        df.dropna(how='any',inplace= True)
+        return df 
+    
+    def convert_product_weights(self,df,column_name):
+        df[column_name] = df[column_name].apply[self.get_grams]
+        return df
+    
+    def clean_products_data(self,df):
+        df = self.clean_invalid_date(df,'date_added')
+        df.dropna(how='any',inplace=True)
+        return df
+
+
+    def clean_orders_data(self,df):
+        df.drop(columns='1',inplace=True)
+        df.drop(columns='first_name',inplace=True)
+        df.drop(columns='last_name',inplace=True)
+        df.drop(columns='level_0',inplace=True)
+        df['card_number'] = df['card_number'].apply(self.isDigits)
+        df.dropna(how='any',inplace= True)
+        return df
+    
+    def isDigits(self,num):
+        return str(num) if str(num).isdigit() else np.nan
+
+
+    def clean_store_data(self,df):
+        df.drop(columns='lat',inplace=True)
+        df =  self.clean_invalid_date(df,'opening_date')                     
+        df['staff_numbers'] =  pd.to_numeric( df['staff_numbers'].apply(self.remove_char_from_string),errors='coerce', downcast="integer") 
+        df.dropna(subset = ['staff_numbers'],how='any',inplace= True)
+        return df
+
+    def get_grams(self,value):
+        value = str(value)
+        value = value.replace('.',)
--- data_extraction.py
+++ data_extraction.py
@@ -1,1 +1,76 @@
+from sqlalchemy import inspect
+import boto3
+import pandas as pd
+import tabula
+import requests
+link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
+
+
+
+def retrieve_pdf_data(self,link):
+    return pd.concat(tabula.read_pdf(link, pages='all'))
+
+# run = tabula.read_pdf('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf',pages=all)
+# print(run)
+
 class DataExtractor:
+   
+   def __init__(self):
+      pass
+
+   def read_rds_table(self,engine,table_name):
+      with engine.connect() as conn:
+         return pd.read_sql_table(table_name, con=conn)
+      
+   def retrieve_pdf_data(self,link):
+    return pd.concat(tabula.read_pdf(link, pages='all',stream=False))
+      
+   
+   def API_key(self):
+      return {'x-api-key':'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX' }
+   
+   def list_number_of_stores(self):
+      api_url_base = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
+      response = requests.get(api_url_base,headers=self.API_key())
+      return response.json()['number_stores']
+   
+
+   def retrieve_stores_data(self):
+      list_of_frames = []
+      store_number   = self.list_number_of_stores()
+      for i in range(store_number):
+            api_url_base = f'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{_}'
+            response = requests.get(api_url_base,headers=self.API_key())
+            list_of_frames.append( pd.json_normalize(response.json()))
+      return pd.concat(list_of_frames)
+   
+   def extract_from_s3(self):
+      s3_client = boto3.client(3)
+      response = s3_client.get_object(Bucket='data-handling-public',key = 'products.csv')
+      status  = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+      if status == 200:
+         print(f'success!{status}')
+         return pd.read_csv(response.get('body'))
+      else:
+         print(f'Unsuccesful{status}')
+
+    
+   def extract_from_s3_link(self):
+      url = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
+      response = requests.get(url)
+      dic = response.json
+      df = pd.DataFrame([])
+      for column_name in dic.keys():
+         value_list = []
+         for i in dic[column_name].keys():
+            value_list.append(dic[column_name][_])
+            df[column_name] = value_list
+      return df
+      
+      
+
+
+
+
+
+      

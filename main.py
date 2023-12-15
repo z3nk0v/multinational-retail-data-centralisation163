ceac11de29fd6ca2ad4ba768e68f751b6697c27f
Dmitry Rukazenkov <dmitry@Dmitrys-MacBook-Air.local>
2023-12-15 00:23:27 +0000

    main updates

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ db = DatabaseConnector()
 dc = DataCleaning() 
 
 def upload_dim_users():
-'''This function connects to our engine on aws using the details stored in our db_creds file then returns a list of tables 
-we then clean the table data removing null values and upload this into our local engine as dim users'''
+    '''This function connects to our engine on aws using the details stored in our db_creds file then returns a list of tables 
+    we then clean the table data removing null values and upload this into our local engine as dim users'''
 
 
     cred = db.read_db_creds('db_creds.yaml')
@@ -36,11 +36,8 @@ upload_dim_users()
 
 def upload_dim_card_details():
     df = pd.read_csv('cards.csv')
-    #df = de.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
-    print(df.head())
-    print(df.info())
+    #df = de.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')- this didnt work for some reason so had to use a local csv file
     df = dc.clean_card_data(df)
-    #show(df)
     cred = db.read_db_creds('local_details.yaml')
     engine = db.init_db_engine(cred)
     engine.connect()
@@ -75,10 +72,7 @@ def upload_dim_products():
     engine.connect()
     db.upload_to_db(df,'dim_products', engine)
     print(engine)
-#upload_dim_products()
-
-
-
+upload_dim_products()
 
 
 
@@ -93,7 +87,6 @@ def upload_orders():
     df.to_csv('orders_table.csv')    
     # converts to csv
     df = dc.clean_orders_data(df)
-    print(df['product_quantity'].sum())
     # upload to our postgres server 
     cred   = db.read_db_creds("local_details.yaml") 
     engine = db.init_db_engine(cred)
@@ -108,8 +101,10 @@ upload_orders()
 
 def dim_date_times():
     df = de.extract_json_from_s3()
+    #calls our function to extract from s3
     df.to_csv('dim_date_times.csv')
     df = dc.clean_date_time(df)
+    #cleans file
     cred   = db.read_db_creds("local_details.yaml") 
     engine = db.init_db_engine(cred)
     engine.connect()
